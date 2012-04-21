19fb555ad796f1ca4c8a0ceb9b2b2fae7559c1ca
Stephan Schmitz <eyecatchup@gmail.com>
2012-04-21 13:08:48 +0200

    Initial commit.

--- youtube-dl.class.php
+++ youtube-dl.class.php
@@ -155,17 +155,17 @@ class yt_downloader implements cnfg
 	private function parse_yturl($url) 
 	{
 		$pattern = '#^(?:https?://)?';		# Optional URL scheme. Either http or https.
-		$pattern .= '(?:www\.)?';			# Optional www subdomain.
-		$pattern .= '(?:'; 					# Group host alternatives:
-		$pattern .=   'youtu\.be/';			#   Either youtu.be,
+		$pattern .= '(?:www\.)?';		# Optional www subdomain.
+		$pattern .= '(?:'; 			# Group host alternatives:
+		$pattern .=   'youtu\.be/';		#   Either youtu.be,
 		$pattern .=   '|youtube\.com';		#   or youtube.com
-		$pattern .=   '(?:';				# Group path alternatives:
-		$pattern .=     '/embed/';			#   Either /embed/,
-		$pattern .=     '|/v/';				#   or /v/,
+		$pattern .=   '(?:';			# Group path alternatives:
+		$pattern .=     '/embed/';		#   Either /embed/,
+		$pattern .=     '|/v/';			#   or /v/,
 		$pattern .=     '|/watch\?v=';		#   or /watch?v=,	
 		$pattern .=     '|/watch\?.+&v=';	#   or /watch?other_param&v=
-		$pattern .=   ')';					# End path alternatives.
-		$pattern .= ')';					# End host alternatives.
+		$pattern .=   ')';			# End path alternatives.
+		$pattern .= ')';			# End host alternatives.
 		$pattern .= '([\w-]{11})';     		# 11 characters (Length of Youtube video ids).
 		$pattern .= '(?:.+)?$#x';     		# Optional other ending URL parameters.
 		preg_match($pattern, $url, $matches);
